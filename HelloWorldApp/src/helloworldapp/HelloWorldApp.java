1686b6d3281a115e0f7bbbc0c59e20b8b9fb36eb
Gerardo Mathus <gmathus@Gerardos-MacBook-Pro.local>
2016-05-15 17:50:40 -0500

    Innitial commit

---
+++ HelloWorldApp/src/helloworldapp/HelloWorldApp.java
@@ -0,0 +1,12 @@
+package helloworldapp;
+
+/**
+ *
+ * @authors GeMaths & jcfontecha
+ */
+public class HelloWorldApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+}
